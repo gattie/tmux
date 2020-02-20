eb0d8b4cb4b22d42028bc8ce57f72fb1ef81cb93
Matt Gaspar <mgaspar@godaddy.com>
2020-02-19 17:18:05 -0700

    Update stock.py to accept stock symbol arg

--- stock.py
+++ stock.py
@@ -1,11 +1,25 @@
 import requests
+import sys
 
-result = requests.get("https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=^GSPC")
+stock_symbol = sys.argv[1]
+
+result = requests.get(
+    ("https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&"
+    "corsDomain=finance.yahoo.com&symbols={}").format(stock_symbol)
+    )
 
 quote_response = result.json()['quoteResponse']['result'][0]
 
 if quote_response['regularMarketChange'] > 0:
-    print("#[fg=#66ff00]{} ({})".format(quote_response['regularMarketPrice'], round(quote_response['regularMarketChange'], 2)))
+    print("#[fg=black]{}: #[fg=#66ff00]{} ({})".format(
+        stock_symbol,
+        quote_response['regularMarketPrice'],
+        round(quote_response['regularMarketChange'], 2)
+        ))
 else:
-    print("#[fg=#ff0000]{} ({})".format(quote_response['regularMarketPrice'], round(quote_response['regularMarketChange'], 2)))
+    print("#[fg=black]{}: #[fg=#ff0000]{} ({})".format(
+        stock_symbol,
+        quote_response['regularMarketPrice'],
+        round(quote_response['regularMarketChange'], 2)
+        ))
 

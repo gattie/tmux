fc38c717c3e0ebfc48f03c8e1f1c267382f9419a
Matt Gaspar <mgaspar@godaddy.com>
2020-03-08 20:28:55 -0700

    Initial commit for weather.py

---
+++ weather.py
@@ -0,0 +1,147 @@
+import requests
+import argparse
+import json
+import urllib.parse
+
+API_KEY=""
+
+def weather_icon(weather_category):
+    if 200 <= weather_category <= 299:
+        weather_category = 200
+    elif 300 <= weather_category <= 399:
+        weather_category = 300
+    elif 500 <= weather_category <= 599:
+        weather_category = 500
+    elif 600 <= weather_category <= 699:
+        weather_category = 600
+    elif 700 <= weather_category <= 799:
+        weather_category = 700
+
+    icons = {
+        200: b'\xe2\x9b\x88',             # Thunderstorm
+        300: b'\xf0\x9f\x8c\xa6',         # Drizzle
+        500: b'\xf0\x9f\x8c\xa7',         # Rain
+        600: b'\xf0\x9f\x8c\xa8',         # Snow
+        700: b'\xf0\x9f\x92\xa8',         # Atmosphere (Smoke, Haze, Fog, etc.)
+        800: b'\xe2\x98\x80\xef\xb8\x8f', # Clear
+        801: b'\xe2\x9b\x85',             # Few Clouds
+        802: b'\xe2\x9b\x85\xef\xb8\x8f', # Scattered Clouds
+        803: b'\xe2\x98\x81\xef\xb8\x8f', # Broken Clouds
+        804: b'\xe2\x98\x81\xef\xb8\x8f', # Overcast Clouds
+    }
+
+    value = icons.get(weather_category)
+
+    return value
+
+def geocode(address):
+    geo_data = requests.get("https://geocode.xyz/{}?json=1".format(
+        urllib.parse.quote_plus(address)))
+    geo_json = json.loads(geo_data.content)
+
+    return geo_json['standard']['city'], geo_json['latt'], geo_json['longt']
+
+def get_weather(latitude, longitude, units):
+    """
+    {'base': 'stations',
+     'clouds': {'all': 20},
+     'cod': 200,
+     'coord': {'lat': 33.32, 'lon': -111.73},
+     'dt': 1583720426,
+     'id': 5295903,
+     'main': {'feels_like': 16.11,
+              'humidity': 40,
+              'pressure': 1015,
+              'temp': 19.62,
+              'temp_max': 21.67,
+              'temp_min': 17.22},
+     'name': 'Gilbert',
+     'sys': {'country': 'US',
+             'id': 4553,
+             'sunrise': 1583675146,
+             'sunset': 1583717374,
+             'type': 1},
+     'timezone': -25200,
+     'visibility': 16093,
+     'weather': [{'description': 'few clouds',
+                  'icon': '02n',
+                  'id': 801,
+                  'main': 'Clouds'}],
+     'wind': {'deg': 270, 'speed': 3.6}}
+
+    Arguments:
+        latitude {[type]} -- [description]
+        longitude {[type]} -- [description]
+        units {[type]} -- [description]
+    """
+    global API_KEY
+    qs = {
+        'lat': latitude,
+        'lon': longitude,
+        'APPID': API_KEY,
+        'units': units
+    }
+    qs = urllib.parse.urlencode(qs)
+    url = "http://api.openweathermap.org/data/2.5/weather?{}".format(qs)
+    weather = requests.get(url)
+    weather_json = json.loads(weather.content)
+    category = weather_json['weather'][0]['id']
+    temp = weather_json['main']['temp']
+    wind_speed = weather_json['wind']['speed']
+
+    return category, temp, wind_speed
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c',
+        dest='city',
+        help="The display name of the weather location"
+    )
+    parser.add_argument(
+        '-a',
+        dest='address',
+        help="Look up geocode by street address \
+            (e.g. 3400 E Sky Harbor Blvd, Phoenix, AZ 85034)"
+    )
+    parser.add_argument(
+        '-l',
+        dest='latitude',
+        help="Latitude coordinates of weather location"
+    )
+    parser.add_argument(
+        '-L',
+        dest='longitude',
+        help="Longitude coordinates of weather location"
+    )
+    parser.add_argument(
+        '-u',
+        dest='units',
+        choices=['metric', 'imperial'],
+        default='metric',
+        help="celcius or farenheit"
+    )
+    args = parser.parse_args()
+
+    temp_unit = 'C' if args.units == 'metric' else 'F'
+
+    if args.address:
+        city, lat, lon = geocode(args.address)
+        print("python {} -c {} -l {} -L {}".format(
+            parser.prog, city.replace(" ", "\\ "), lat, lon))
+    else:
+        category, temp, wind_speed = get_weather(
+            args.latitude, args.longitude, args.units)
+        icon = weather_icon(category)
+        # print(icon.decode('utf-8'))
+        print("{}:{}  {}°{}, {}ms".format(
+            args.city,
+            icon.decode('utf-8'),
+            round(temp),
+            temp_unit,
+            round(wind_speed)
+        ))
+
+if __name__ == "__main__":
+    main()
